7d2e4148718b1ab0a9de64c3329d23731ac7679b
LeandroHurtado <leandro_david@hotmail.es>
2022-05-04 19:52:45 -0300

    chore: clear

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <h1>hola mundo</h1>
+      <h1>hola mundoo</h1>
     </div>
   );
 }
