fd5b877a3036fd0a2e3ca4c47bcb973cec87d8ed
LeandroHurtado <leandro_david@hotmail.es>
2022-05-13 21:27:57 -0300

    productCard-Merge

--- src/routes/appRoute.js
+++ src/routes/appRoute.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
 import ContactPage from "../pages/ContacsPage";
@@ -9,12 +8,10 @@ import NotFound from "../pages/NotFound";
 const App = () => {
   return (
     <Router>
-      {/* esto mas adelante debe ser modificado */}
-      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products:id" element={<ProductPage />} />
+        <Route path="/products/:id" element={<ProductPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="*" element={<NotFound />} />
