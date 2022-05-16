7071db36193e4445b9af3caa0a0c23ff9f0eba4c
facu410 <facundito410@gmail.com>
2022-05-15 21:36:19 -0300

    chore:footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import AppRoute from "./routes/appRoute";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const App = () => {
         {/* esto mas adelante debe ser modificado */}
         <Navbar />
         <AppRoute />
+        <Footer/>
       </ChakraProvider>
 
     </>
